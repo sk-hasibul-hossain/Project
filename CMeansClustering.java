3277716fac18b256f5a0048ac49c2dec35f2ee43
Hasibul <skhasibulhossain16@gmail.com>
2023-04-28 08:10:40 +0530

    all functions are functional

--- CMeansClustering.java
+++ CMeansClustering.java
@@ -5,6 +5,7 @@ import javax.imageio.ImageIO;
 //import net.sf.javaml.distance.EuclideanDistance;
 
 import java.io.File;
+import java.util.*;
 //import java.io.IOException;
 
 class Image {
@@ -68,6 +69,10 @@ class EuclideanDistance {
     int width = 0;
     int height = 0;
     int k;
+    int positionOfVj = 0;
+
+    List<List<Double>> vj = new LinkedList<>(); // cluster points
+    List<List<Double>> uij = new LinkedList<>();
 
     EuclideanDistance(BufferedImage image, int k) {
         this.image = image;
@@ -101,19 +106,36 @@ class EuclideanDistance {
         return clusterPoints;
     }
 
+    public double euclidianDistance(double clusterCenter, double actualPixel) {
+        double distance = Math.abs(actualPixel - clusterCenter);
+        return distance;
+    }
 }
 
 class MemberShipCluster extends EuclideanDistance {
     // BufferedImage image;
     // int numberOfCluster;
     // super(image);
+    int m;
+    double bita;
 
-    MemberShipCluster(BufferedImage image, int k) {
-        // this.image = image;
+    MemberShipCluster(BufferedImage image, int k, int m, double bita) {
         super(image, k);
-        // EuclideanDistance objectOfEuclideanDistance = new EuclideanDistance(image,
-        // numberOfCluster);
-        // int clusterPoints[][] = objectOfEuclideanDistance.RandomChoseCluster();
+        this.m = m;
+        this.bita = bita;
+    }
+
+    // intially store the random k point values
+    public void initallyPutKValues() {
+        List<Double> helpList = new LinkedList<>();
+        int randomPintsArr[][] = RandomChoseCluster();
+        for (int i = 0; i < k; i++) {
+            int x = randomPintsArr[i][0];
+            int y = randomPintsArr[i][1];
+            double px = imagePixelExtraction(x, y);
+            helpList.add(px);
+        }
+        vj.add(helpList);
     }
 
     // int sizeOfClusterPointsArray = clusterPoints.length;
@@ -126,20 +148,61 @@ class MemberShipCluster extends EuclideanDistance {
         }
     }
 
+    public double fuzzyMembership(double pixel) {
+        double u = 0.0;
+        List<Double> list = vj.get(0);
+        for (int i = 0; i < k; i++) {
+            u += Math.pow((pixel / euclidianDistance(list.get(i), pixel)), (2 / (m - 1)));
+        }
+        return ((1 * 1.0) / u);
+    }
+
+    // invoke pixel one by one we get fuzzy centers vj
+    public void fuzzyCenters() {
+        int x = width;
+        double u1 = 0.0, u2 = 0.0;
+        for (int i = 0; i < x; i++) {
+            List<Double> helpList = new LinkedList<>();
+            for (int j = 0; j < height; j++) {
+                double fuzzyMembershipValue = fuzzyMembership(imagePixelExtraction(i, j));
+                helpList.add(fuzzyMembershipValue);
+                u1 = fuzzyMembershipValue * imagePixelExtraction(i, j) + u1;
+                u2 += fuzzyMembershipValue;
+                // uij.get(i).add(fuzzyMembership(imagePixelExtraction(i, j)));
+            }
+            // System.out.println(i);
+            uij.add(helpList);
+        }
+        System.out.println(u1 / u2);// we can see the result by using print
+        // we have to add in vj
+    }
+
+    public double imagePixelExtraction(int x, int y) {
+        int p = image.getRGB(x, y);
+        // System.out.print(" " + p + " ");
+        int a = (p >> 24) & 0xff;
+        double r = (p >> 16) & 0xff;
+        // int g = (p >> 8) & 0xff;
+        // int b = (p & 0xff);
+        return r;
+    }
 }
 
 public class CMeansClustering {
 
     public static void main(String args[]) {
-        GrayScale g = new GrayScale("./selected.jpg", "selectedGrayscaleForCMeansCluster.jpg");
+        GrayScale g = new GrayScale("./m.jpg", "selectedGrayscaleForCMeansCluster.jpg");
         BufferedImage image = g.image;
         int numberOfCluster = 3;
-        MemberShipCluster objectOfMemberShipCluster = new MemberShipCluster(image, numberOfCluster);
+        int m = 2; // fuzziness
+        double bita = 0.7; // bita range between 0-1
+        MemberShipCluster objectOfMemberShipCluster = new MemberShipCluster(image, numberOfCluster, m, bita);
         // objectOfMemberShipCluster.image = image;
         // objectOfMemberShipCluster.numberOfCluster = 3;
         // objectOfMemberShipCluster.RandomChoseCluster();
         // new MemberShipCluster().display();
-        objectOfMemberShipCluster.display();
-
+        // objectOfMemberShipCluster.display();
+        objectOfMemberShipCluster.initallyPutKValues();
+        objectOfMemberShipCluster.fuzzyCenters();
     }
 }
